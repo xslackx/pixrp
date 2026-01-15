e154b69a4303e2d62ca98e8fd87ee7ccfc7243d0
Ruan <xslackx@live.fr>
2026-01-15 11:12:51 -0300

    plus which type of def and plus return for else

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ from genpix import tx_user
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_world() -> str:
     return "<p>usage: /pix?txid=RP4205F31626541746F653CBAD3VBECDBAX&value=1/p>"
 
 @app.route('/pix', methods=['GET'])
-def generate_static_pix():
+def generate_static_pix() -> dict:
     user_txid = request.args.get('txid')
     user_value = request.args.get('value')
     if user_txid and user_value:
-        return tx_user(user_txid, user_value)
-    return
+        return {
+                "status": 'ok', 
+                'code': tx_user(user_txid,user_value)
+                }
+    else: 
+        return {"status": "fail", "code": ""}
     
